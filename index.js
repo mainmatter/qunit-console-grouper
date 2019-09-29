b690d7ae31ce2713d3559aa070b1575a0025956b
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-09-29 17:43:45 +0200

    Add support for boolean `QUNIT_CONSOLE_GROUPER` environment variable

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ module.exports = {
   name: 'qunit-console-grouper',
 
   isEnabled() {
+    // support `QUNIT_CONSOLE_GROUPER` environment variable: true/false
+    if (process.env.QUNIT_CONSOLE_GROUPER) {
+      return process.env.QUNIT_CONSOLE_GROUPER === 'true';
+    }
+
     if (process.env.EMBER_CLI_TEST_COMMAND === 'true') {
       // disable this addon for `ember test`, but not `ember test --server`
       return (
