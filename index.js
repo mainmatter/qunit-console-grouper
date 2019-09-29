431326c3205e4a7737679f4186493462b045bd17
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-09-29 17:42:16 +0200

    Disable addon for `ember test`, but not `ember test --server`

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const Funnel = require('broccoli-funnel');
 module.exports = {
   name: 'qunit-console-grouper',
 
+  isEnabled() {
+    if (process.env.EMBER_CLI_TEST_COMMAND === 'true') {
+      // disable this addon for `ember test`, but not `ember test --server`
+      return (
+        process.argv.includes('--server') ||
+        process.argv.includes('-server') ||
+        process.argv.includes('--serve') ||
+        process.argv.includes('-serve') ||
+        process.argv.includes('-s')
+      );
+    }
+
+    return true;
+  },
+
   included() {
     this._super.included.apply(this, arguments);
     this.import('vendor/qunit-console-grouper.js', { type: 'test' });
