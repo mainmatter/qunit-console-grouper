ea89686da4d693a92a0f2b7e33ee6485aa3d60b9
Patsy Issa <patsy.issa@gmail.com>
2020-05-14 17:20:27 +0200

    Upgrade prettier (#121)
    * Upgrade prettier
    
    * Fix lint errors

--- tests/example-test.js
+++ tests/example-test.js
@@ -1,28 +1,28 @@
 import { module, test } from 'qunit';
 
-module('first suite', function() {
-  test('test', function(assert) {
+module('first suite', function () {
+  test('test', function (assert) {
     assert.ok(true);
   });
 });
 
-module('second suite', function() {
-  test('test', function(assert) {
+module('second suite', function () {
+  test('test', function (assert) {
     assert.ok(true);
   });
 });
 
-module('foo', function() {
-  test('bar', function(assert) {
+module('foo', function () {
+  test('bar', function (assert) {
     console.log('baz');
     assert.ok(true);
   });
 
-  test('42', function(assert) {
+  test('42', function (assert) {
     assert.ok(true);
   });
 
-  test('41', function(assert) {
+  test('41', function (assert) {
     assert.ok(true);
   });
 });
